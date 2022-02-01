3749f85e8a830be05c49c20b14ec0684c381456c
Hablitzel, Jonas AVL/DE <jonas.hablitzel@avl.com>
2022-02-01 16:38:42 +0100

    added spotify

--- js/app.js
+++ js/app.js
@@ -869,3 +869,37 @@ function sendtopopup() {
     PopIntenstitle.style.color = color;
   }
 }
+
+
+// Spotify
+
+function generateRandomString(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
+
+
+var client_id = 'd3ea418eb4c94e19835b0666dfbb079a';
+
+//var redirect_uri = 'https://cycle-circle.vercel.app/';
+var redirect_uri = 'http://localhost:3000/'
+
+var state = generateRandomString(16);
+
+// localStorage.setItem(stateKey, state);
+var scope = 'user-read-private user-read-email';
+
+var url = 'https://accounts.spotify.com/authorize';
+url += '?response_type=token';
+url += '&client_id=' + encodeURIComponent(client_id);
+url += '&scope=' + encodeURIComponent(scope);
+url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+url += '&state=' + encodeURIComponent(state);
+
+window.location.href = url;
